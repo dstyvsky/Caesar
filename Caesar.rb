3c8cc510f7267c6c2b395cb7c4646c84a54aa4e0
Fyodor <dstyvsky@gmail.com>
2015-09-06 19:09:51 -0400

    Just need to add capital letters

--- Caesar.rb
+++ Caesar.rb
@@ -1,13 +1,29 @@
 puts "Please enter a sentence to encode"
-
 sentence = gets.chomp
 
-
+puts "Please enter a key"
+key = gets.chomp
 
 letters = sentence.split('')
+key = key.to_i
+while key > 26 do
+	key -= 26
+end
+tkey = 0
+
 
 letters.each do |x| 
-	x.ord == 32 ? (print x) : (print (x.ord + 1).chr)
+	tkey = key
+	if x.ord == 32  
+		(print x)
+	else
+		if x.ord + key >122
+			tkey -= 26
+		end
+
+		(print (x.ord + tkey).chr)
+		print "(#{x.ord}) "
+	end
 end
 
 puts ""
